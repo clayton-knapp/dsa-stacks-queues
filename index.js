f7bab74bf4fe940c1c47be7d05980349b6994a2e
Clayton Knapp <csjknapp@gmail.com>
2022-04-05 15:01:08 -0700

    add node function

--- index.js
+++ index.js
@@ -18,18 +18,18 @@ class Stack {
     return this.stack[this.stack.length - 1];
   }
 }
-const stack = new Stack();
-stack.push('fox');
-stack.push('goose');
-stack.push('lizard');
-console.log(stack.pop()); // 'lizard'
-console.log(stack.peek()); // 'goose'
-console.log(stack.pop()); // 'goose'
-stack.push('llama');
-console.log(stack.pop()); // 'llama'
-console.log(stack.peek()); // 'fox'
-console.log(stack.pop()); // 'fox'
-console.log(stack.pop()); // null
+// const stack = new Stack();
+// stack.push('fox');
+// stack.push('goose');
+// stack.push('lizard');
+// console.log(stack.pop()); // 'lizard'
+// console.log(stack.peek()); // 'goose'
+// console.log(stack.pop()); // 'goose'
+// stack.push('llama');
+// console.log(stack.pop()); // 'llama'
+// console.log(stack.peek()); // 'fox'
+// console.log(stack.pop()); // 'fox'
+// console.log(stack.pop()); // null
 
 
 
@@ -63,54 +63,93 @@ class Queue {
 
 
 
-const queue = new Queue();
-queue.enqueue('fox');
-queue.enqueue('goose');
-queue.enqueue('lizard');
-console.log('queue', queue);
-console.log(queue.dequeue()); // 'fox'
-console.log(queue.hasNext()); // true
-console.log(queue.dequeue()); // 'goose'
-queue.enqueue('llama');
-console.log(queue.dequeue()); // 'lizard'
-console.log(queue.hasNext()); // true
-console.log(queue.dequeue()); // 'llama'
-console.log(queue.hasNext()); // false
-console.log(queue.dequeue()); // null
+// const queue = new Queue();
+// queue.enqueue('fox');
+// queue.enqueue('goose');
+// queue.enqueue('lizard');
+// console.log('queue', queue);
+// console.log(queue.dequeue()); // 'fox'
+// console.log(queue.hasNext()); // true
+// console.log(queue.dequeue()); // 'goose'
+// queue.enqueue('llama');
+// console.log(queue.dequeue()); // 'lizard'
+// console.log(queue.hasNext()); // true
+// console.log(queue.dequeue()); // 'llama'
+// console.log(queue.hasNext()); // false
+// console.log(queue.dequeue()); // null
 
 
 
 
 // Dan's Queue
 
-class Queue2 {
-  #list = [];
+// class Queue2 {
+//   #list = [];
 
-  enqueue(item) {
-    this.#list.push(item);
+//   enqueue(item) {
+//     this.#list.push(item);
+//   }
+
+//   dequeue() {
+//     return this.#list.length ? this.#list.shift() : null;
+//   }
+
+//   hasNext() {
+//     return !!this.#list.length; //!! returns booleanized value
+//   }
+// }
+
+
+// const queue2 = new Queue2();
+// queue2.enqueue('fox');
+// queue2.enqueue('goose');
+// queue2.enqueue('lizard');
+// console.log('queue', queue2);
+// console.log(queue2.dequeue()); // 'fox'
+// console.log(queue2.hasNext()); // true
+// console.log(queue2.dequeue()); // 'goose'
+// queue2.enqueue('llama');
+// console.log(queue2.dequeue()); // 'lizard'
+// console.log(queue2.hasNext()); // true
+// console.log(queue2.dequeue()); // 'llama'
+// console.log(queue2.hasNext()); // false
+// console.log(queue2.dequeue()); // null
+
+
+
+class LinkedListNode {
+  // #data = '';
+  // #next = '';
+
+  constructor(data)
+  {
+    this.data = data;
+    this.next = null; //another instance of LinkedListNode initialized as null
   }
 
-  dequeue() {
-    return this.#list.length ? this.#list.shift() : null;
-  }
-
-  hasNext() {
-    return !!this.#list.length; //!! returns booleanized value
+  add(node) {
+    // check to see if there is already a next property
+    if (!this.next) {
+      //if not, this passed node can be our next in the LinkedList
+      this.next = node;
+    }
+    else {
+      //if there is already a node, run add method recursively on this.next and pass in initial node
+      this.next.add(node);
+    }
   }
 }
 
-
-const queue2 = new Queue2();
-queue2.enqueue('fox');
-queue2.enqueue('goose');
-queue2.enqueue('lizard');
-console.log('queue', queue2);
-console.log(queue2.dequeue()); // 'fox'
-console.log(queue2.hasNext()); // true
-console.log(queue2.dequeue()); // 'goose'
-queue2.enqueue('llama');
-console.log(queue2.dequeue()); // 'lizard'
-console.log(queue2.hasNext()); // true
-console.log(queue2.dequeue()); // 'llama'
-console.log(queue2.hasNext()); // false
-console.log(queue2.dequeue()); // null
+const root = new LinkedListNode('A');
+const nodeB = new LinkedListNode('B');
+root.add(nodeB);
+console.log('root', root);
+// console.log(root.getList()); // 'A B'
+const nodeC = new LinkedListNode('C');
+const nodeD = new LinkedListNode('D');
+const nodeE = new LinkedListNode('E');
+root.add(nodeC);
+root.add(nodeD);
+root.add(nodeE);
+console.log('nodeD', nodeD);
+// console.log(root.getList()); // 'A B C D E'
