22ac3b082224b27de34976f97fe142a546c6a2dc
Clayton Knapp <csjknapp@gmail.com>
2022-04-05 17:56:43 -0700

    added add function for person binary tree

--- index.js
+++ index.js
@@ -154,16 +154,114 @@ class LinkedListNode {
 
 }
 
-const root = new LinkedListNode('A');
-const nodeB = new LinkedListNode('B');
-root.add(nodeB);
-console.log('root', root);
-console.log(root.getList()); // 'A B'
-const nodeC = new LinkedListNode('C');
-const nodeD = new LinkedListNode('D');
-const nodeE = new LinkedListNode('E');
-root.add(nodeC);
-root.add(nodeD);
-root.add(nodeE);
-console.log('nodeD', nodeD);
-console.log(root.getList()); // 'A B C D E'
+// const root = new LinkedListNode('A');
+// const nodeB = new LinkedListNode('B');
+// root.add(nodeB);
+// console.log('root', root);
+// console.log(root.getList()); // 'A B'
+// const nodeC = new LinkedListNode('C');
+// const nodeD = new LinkedListNode('D');
+// const nodeE = new LinkedListNode('E');
+// root.add(nodeC);
+// root.add(nodeD);
+// root.add(nodeE);
+// console.log('nodeD', nodeD);
+// console.log(root.getList()); // 'A B C D E'
+
+
+
+
+
+
+class BinaryTreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+  
+  add(node) {
+    if(node.value === this.value) {
+      //edge case if duplicate value just return cuz already in tree
+      return;
+    }
+    if(node.value < this.value) {
+      if(!this.left) this.left = node;
+      else this.left.add(node);
+    }
+    else {
+      if(!this.right) this.right = node;
+      else this.right.add(node);
+    }
+  }
+}
+
+
+const B = new BinaryTreeNode('B');
+const A = new BinaryTreeNode('A');
+const C = new BinaryTreeNode('C');
+const D = new BinaryTreeNode('D');
+
+// B will be the root of the tree:
+B.add(A);
+B.add(D);
+B.add(C);
+console.log(B);
+
+
+
+
+// BINARY NODE FIND PERSON
+
+class PersonTreeNode {
+  constructor(person) {
+    this.value = person.name;
+    this.person = person;
+    this.left = null;
+    this.right = null;
+  }
+
+  add(node) {
+    // implemented as in previous challenge
+    if(node.value < this.value) {
+      if(!this.left) this.left = node;
+      else this.left.add(node);
+    }
+    else {
+      if(!this.right) this.right = node;
+      else this.right.add(node);
+    }
+  }
+
+  findPerson(name) {
+    // Implement me!
+    if(name === this.value) return this.value;
+
+    if(name < this.value) {
+      if(name === this.left) return this.left;
+      else this.left.findPerson(name);
+    }
+
+    else {
+      if(name === this.right) return this.right;
+      else this.right.findPerson(name);
+    }
+
+    }
+}
+
+
+const Blake = new PersonTreeNode({ name: 'Blake' });
+const Amy = new PersonTreeNode({ name: 'Amy' });
+const Cliff = new PersonTreeNode({ name: 'Cliff', cool: 'dude' });
+const Dave = new PersonTreeNode({ name: 'Dave' });
+
+// B will be the root of the tree:
+Blake.add(Amy);
+Blake.add(Dave);
+Blake.add(Cliff);
+
+// console.log('Blake', Blake);
+
+
+console.log(Blake.findPerson('Cliff'));
